897b1edfb7d19a6cdf60c43ad73867bb10d9bd1a
Simonas Tunaitis <simonas.tunaitis@gmail.com>
2023-06-30 18:32:06 +0300

    Initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,47 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'doums/darcula'
+  use 'lunarvim/horizon.nvim'
+
+  use 'tpope/vim-vinegar'
+  use 'junegunn/fzf'
+  use 'junegunn/fzf.vim'
+
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use 'ernstvanderlinden/vim-coldfusion'
+
+  use 'sbdchd/neoformat'
+
+  use 'neovim/nvim-lspconfig'
+
+end)
