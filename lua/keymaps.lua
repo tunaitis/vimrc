41f5ffe7ade3e035664dec06f8c9b04900f99da6
simtun <simonas.tunaitis@gmail.com>
2023-07-06 09:12:43 +0200

    Move telescope key bindings to the main keymaps file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,9 @@
 -- set space as leader key
 vim.g.mapleader = ' '
+
+vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
+vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
