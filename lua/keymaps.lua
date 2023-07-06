8efc049e1bb7d91b14d1939a8ecbc5c27c9ca397
simtun <simonas.tunaitis@gmail.com>
2023-07-06 15:53:52 +0200

    Add which-key

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,21 @@
 -- set space as leader key
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
-vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
-vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+local wk = require("which-key")
+
+wk.register({
+  ["<leader>"] = {
+    ["<tab>"] = { "<cmd>bp<cr>", "Previous Buffer" },
+    ["<leader>"] = { "<cmd>Telescope find_files<cr>", "Open File" },
+    b = { "<cmd>Telescope buffers<cr>", "Switch Buffer" },
+    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent" },
+    n = { "<cmd>enew<cr>", "New File" },
+    f = {
+      name = "Find",
+      f = { "<cmd>Telescope find_files<cr>", "Find File" },
+      c = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Find in Current Buffer" },
+      b = { "<cmd>Telescope buffers<cr>", "Find Buffer" },
+      r = { "<cmd>Telescope oldfiles<cr>", "Find Recent File" },
+    },
+  },
+})
