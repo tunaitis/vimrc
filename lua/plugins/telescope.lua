fb834b5036fffa6c9c8df6f344d5e4469613ebf6
simtun <simonas.tunaitis@gmail.com>
2023-07-06 09:22:30 +0200

    Convert telescope module to object literals

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,7 @@
-local Plugin = {'nvim-telescope/telescope.nvim'}
-
-Plugin.branch = '0.1.x'
-
-Plugin.dependencies = {
-  {'nvim-lua/plenary.nvim'},
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
 }
-
-function Plugin.init()
-  -- See :help telescope.builtin
-end
-
-function Plugin.config()
-  require('telescope')--.load_extension('fzf')
-end
-
-return Plugin
