597a6805755d5915c061c7e7b9b896ef0ed77916
simtun <simonas.tunaitis@gmail.com>
2023-07-09 11:18:31 +0300

    Add nvim-cmp for better auto completion

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,52 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "dburian/cmp-markdown-link",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+    local cmp = require('cmp')
+
+    local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+    cmp.setup {
+      sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp'},
+        {name = 'buffer', keyword_length = 3},
+        {name = 'luasnip', keyword_length = 2},
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      formatting = {
+        fields = {'menu', 'abbr', 'kind'},
+        format = function(entry, item)
+          local menu_icon = {
+            nvim_lsp = 'λ',
+            luasnip = '⋗',
+            buffer = 'Ω',
+            path = '🖫',
+          }
+
+          item.menu = menu_icon[entry.source.name]
+          return item
+        end,
+      },
+      mapping = {
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+      },
+    }
+
+  end,
+}
