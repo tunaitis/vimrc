7c3c29643058c8041d8405eef612b4c4dad9e95e
simtun <simonas.tunaitis@gmail.com>
2023-07-11 17:11:33 +0300

    Add treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = { "lua", "go" },
+      highlight = { enable = true, }
+    }
+  end
+}
