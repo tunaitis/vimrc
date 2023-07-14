d117e99d819434a1dba602be144ac50a62177ef6
simtun <simonas.tunaitis@gmail.com>
2023-07-14 22:14:51 +0300

    Add autotag plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,18 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = { "lua", "go", "html" },
-      highlight = { enable = true, }
-    }
-  end
+  "windwp/nvim-ts-autotag",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = { "lua", "go", "html" },
+        highlight = {
+          enable = true,
+        },
+        autotag = {
+          enable = true,
+        }
+      }
+    end
+  },
 }
