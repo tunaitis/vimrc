a6b8d81198ab3ee2ccfaca32eaa4f7aff50b4451
simtun <simonas.tunaitis@gmail.com>
2023-07-14 21:47:17 +0300

    Add html treesitter module

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "lua", "go" },
+      ensure_installed = { "lua", "go", "html" },
       highlight = { enable = true, }
     }
   end
