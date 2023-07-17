67aace7f1d90b03e39d11878698ba15ffb3886a4
simtun <simonas.tunaitis@gmail.com>
2023-07-17 21:28:26 +0300

    Add nvim-tree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,7 +9,7 @@ wk.register({
     ["<leader>"] = { "<cmd>Telescope find_files<cr>", "Open File" },
     b = { "<cmd>Telescope buffers<cr>", "Switch Buffer" },
     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent" },
-    e = { "<cmd>Telescope file_browser<cr>", "Explorer" },
+    e = { "<cmd>NvimTreeFocus<cr>", "Explorer" },
     n = { "<cmd>enew<cr>", "New File" },
     f = {
       name = "Find",
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -23,7 +23,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      --vim.cmd([[colorscheme rose-pine]])
+      -- vim.cmd([[colorscheme rose-pine]])
     end,
   },
 }
