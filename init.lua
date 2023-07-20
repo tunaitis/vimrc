d0982324930113a02d360d5c2a0299337fb73faf
simtun <simonas.tunaitis@gmail.com>
2023-07-20 22:21:22 +0300

    Add neovide specific config

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+if vim.g.neovide then
+  require('neovide')
+end
+
 require('settings')
 require('keymaps')
 require('plugin-manager')
---
+++ lua/neovide.lua
@@ -0,0 +1,5 @@
+
+-- disable cursor animation
+vim.g.neovide_cursor_animation_length = 0
+
+vim.o.guifont = "Menlo:h16"
