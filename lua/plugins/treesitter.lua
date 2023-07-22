b15f452bcc181f0f0f5e5ace87e48d26d52cbb45
simtun <simonas.tunaitis@gmail.com>
2023-07-22 12:25:01 +0300

    Add tsx module for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
-        ensure_installed = { "lua", "go", "html", "css", "javascript", "typescript" },
+        ensure_installed = { "lua", "go", "html", "css", "javascript", "typescript", "tsx" },
         highlight = {
           enable = true,
         },
