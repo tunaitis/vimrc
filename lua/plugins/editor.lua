73f263dd7cabf4667b7b1cc41c7bfc8c55588dff
simtun <simonas.tunaitis@gmail.com>
2023-07-24 22:24:46 +0300

    Move everything from treesitter to editor module

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,7 +1,24 @@
 return {
-
   -- custom syntax support
   {
     "Glench/Vim-Jinja2-Syntax",
+  },
+
+  -- treesitter and its plugins
+  "windwp/nvim-ts-autotag",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = { "lua", "go", "html", "css", "javascript", "typescript", "tsx" },
+        highlight = {
+          enable = true,
+        },
+        autotag = {
+          enable = true,
+        }
+      }
+    end
   },
 }
