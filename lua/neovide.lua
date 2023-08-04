b5a8313e0606b11ba2b269e5c1d33838aebcbaaa
simtun <simonas.tunaitis@gmail.com>
2023-08-04 22:38:03 +0300

    Update font settings

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,4 +2,11 @@
 -- disable cursor animation
 vim.g.neovide_cursor_animation_length = 0
 
-vim.o.guifont = "Menlo:h16"
+vim.o.guifont = "Source Code Pro:h15"
+
+vim.g.neovide_padding_top = 0
+vim.g.neovide_padding_bottom = 1
+vim.g.neovide_padding_right = 5
+vim.g.neovide_padding_left = 5
+
+vim.opt.linespace = 2
