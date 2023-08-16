a5085ba87fe0dbf8fc8e038df6ff96df6d9af7a0
simtun <simonas.tunaitis@gmail.com>
2023-08-16 21:53:05 +0100

    Make the trouble window to close automatically after all errors are fixed and improve the trouble keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,6 +12,7 @@ wk.register({
     e = { "<cmd>Telescope file_browser<cr>", "Explorer" },
     n = { "<cmd>enew<cr>", "New File" },
     t = { "<cmd>ToggleTerm<cr>", "Terminal" },
+    x = { "<cmd>Trouble<cr>", "Trouble" },
     f = {
       name = "Find",
       f = { "<cmd>Telescope find_files<cr>", "Find File" },
@@ -24,8 +25,5 @@ wk.register({
   g = {
     name = "Go to",
     d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Go to definition" },
-  },
-  x = {
-    x = { "<cmd>TroubleToggle<cr>",  "Trouble Toggle" },
   },
 })
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -35,5 +35,9 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons"
     },
+    opts = {
+      auto_close = true,
+      auto_preview = false,
+    },
   },
 }
