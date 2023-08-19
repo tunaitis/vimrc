4b7bd4cd7b3f1cc0b0d03c9c91bfe9e7ab601f14
simtun <simonas.tunaitis@gmail.com>
2023-08-19 16:53:55 +0100

    Increase the Neovide's top padding to make it look good with the transparent frame setting on

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -4,7 +4,7 @@ vim.g.neovide_cursor_animation_length = 0
 
 vim.o.guifont = "Consolas:h16"
 
-vim.g.neovide_padding_top = 0
+vim.g.neovide_padding_top = 50
 vim.g.neovide_padding_bottom = 1
 vim.g.neovide_padding_right = 5
 vim.g.neovide_padding_left = 5
